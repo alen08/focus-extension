8dc0fa0a929f22bc9e392e5ff41987c195307d92
alen08 <gegenyi@gmail.com>
2020-06-15 21:39:03 +0200

    Fix Dummy  story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/components/**/*.stories.js'],
+  stories: ['../src/components/**/*.stories.jsx'],
   addons: ['@storybook/addon-actions', '@storybook/addon-links'],
   webpackFinal: async config => {
     // do mutation to the config

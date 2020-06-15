c16604b068c879ee8655ff0c2d469fcecf90a397
alen08 <gegenyi@gmail.com>
2020-06-15 21:57:48 +0200

    Add TypeScript support for stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
-  stories: ['../src/components/**/*.stories.jsx'],
-  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  stories: ['../src/components/**/*.stories.(j|t)sx'],
+  addons: ['@storybook/addon-actions', '@storybook/addon-links', '@storybook/preset-typescript'],
   webpackFinal: async config => {
     // do mutation to the config
 
--- src/components/Dummy/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Dummy = () => {
-  return <div>kaki</div>;
-};
-
-export default Dummy;

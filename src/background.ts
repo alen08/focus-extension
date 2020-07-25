96a96d0fd94d975ac5a4372c042134f576161a7c
alen08 <gegenyi@gmail.com>
2020-07-25 18:53:52 +0200

    Check URLs from an array and add todo

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,8 @@
-const dummyRestrictedUrl = 'https://foo.bar';
+//TODO: Receive restricted URLs from storage
+const dummyRestrictedUrls = ['https://foo.bar', 'https://bar.baz'];
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  if (changeInfo.status === 'loading' && tab.url === dummyRestrictedUrl) {
+  if (changeInfo.status === 'loading' && dummyRestrictedUrls.includes(tab.url)) {
     chrome.tabs.executeScript(tabId, { file: 'content.js' });
   }
 });
